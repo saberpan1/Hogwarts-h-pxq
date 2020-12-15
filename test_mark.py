dd17b615f7bf4f536770f4af4c20b60777527b05
saberpan <saberpan1@163.com>
2020-12-16 00:14:37 +0800

    yaml驱动

--- test_mark.py
+++ test_mark.py
@@ -22,7 +22,9 @@ pytest -v 显示用例名称，不显示print内容
 pytest -s 显示print内容
 pytest -x 有fail就停止执行
 pytest --maxfail=3 fail等于3个时停止执行
-pytest -sv -m "demo or smoke" 执行标签包含demo或 smoke的用例
+pytest -sv -m  "demo or smoke" 执行标签包含demo或 smoke的用例
 
 pytest -sv -m "demo and smoke" 执行标签同时包括demo和smoke的用例
+pytest -sv -k "smoke" test_mark.py 执行方法名包含smoke的用例
+pytest --collect-only test_mark.py 只收集用例，只会把用例名打印出来
 """
