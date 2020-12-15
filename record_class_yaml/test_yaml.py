136d1780539e98cc115ef3b3cfe04a0dbeee57de
saberpan1 <saberpan1@163.com>
2020-12-15 15:08:19 +0800

    录播数据驱动

---
+++ record_class_yaml/test_yaml.py
@@ -0,0 +1,14 @@
+import yaml
+import pytest
+
+
+class TestYaml:
+
+    @pytest.mark.parametrize("env", yaml.safe_load(open("./env.yml")))
+    def test_one(self, env):
+        if "test" in env:
+            print("test环境")
+            print("测试环境的ip是", env["test"])
+        elif "dev" in env:
+            print("dev环境")
+            print("开发环境的ip是", env["dev"])
